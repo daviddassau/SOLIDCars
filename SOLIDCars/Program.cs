f4653251a8466feba9f475f2ee26df78f8b9cd21
David Dassau <todieahero@hotmail.com>
2018-01-30 15:46:14 -0600

    Add project files.

---
+++ SOLIDCars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
